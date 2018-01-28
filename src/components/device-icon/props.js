c5943c958620abebbc33828c0065c4c96cc1d1b7
Jesus Pereyra <jesuspereyrad@gmail.com>
2018-01-27 21:47:22 -0400

    adding props to the device icon component

---
+++ src/components/device-icon/props.js
@@ -0,0 +1,35 @@
+// External
+import PropTypes from 'prop-types';
+
+const MOCK = ['audio', 'camera', 'gaming'];
+
+const propTypes = {
+
+    /*  
+    * Type is an string from an array of string
+    * with possible values store in ../assets/images 
+    */
+    type: PropTypes.oneOfType(MOCK).isRequired,
+
+    /* 
+    * If service is true replace the second position
+    * of image source with service.
+    */
+    service: PropTypes.bool,
+
+    /* 
+    * If power is true replace the third position of 
+    * image source with power.
+    */
+    power: PropTypes.bool
+};
+
+const defaultProps = {
+  power: false,
+  service: false,
+};
+
+export default {
+  propTypes,
+  defaultProps
+};
